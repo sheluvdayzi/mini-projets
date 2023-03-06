028e7bb78266f2fbe67927cce5065c93f266fdd4
sheluvdayzi <ziyadclombe.pro@gmail.com>
2023-03-06 15:08:12 +0100

    infinite scroll terminé

--- debutant/Infinite Scroll/script.js
+++ debutant/Infinite Scroll/script.js
@@ -1,2 +1,9 @@
 let cardGrey = document.querySelector('.grey');
 
+
+
+window.onscroll = function(ev) {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        document.body.appendChild(cardGrey.cloneNode());
+    }
+};
