a8abe45de5f1163cd6bec89d7a51bb7a566127ff
sheluvdayzi <ziyadclombe.pro@gmail.com>
2023-03-07 15:13:57 +0100

    toggling tabs finished

---
+++ debutant/Toggling tabs/assets/js/script.js
@@ -0,0 +1,18 @@
+let click = document.querySelectorAll('.tabs__sort');
+let select = document.querySelector('select');
+
+for(let i = 0; i < click.length; i++){
+    click[i].addEventListener('click', function(){
+        console.log('ok');
+        document.querySelector('.tabs__sort--active').classList.remove('tabs__sort--active');
+        click[i].classList.add('tabs__sort--active');
+        select.selectedIndex = i;
+    });
+
+}
+
+select.addEventListener('click', () => {
+    document.querySelector('.tabs__sort--active').classList.remove('tabs__sort--active');
+    click[select.selectedIndex].classList.add('tabs__sort--active');
+    
+});
