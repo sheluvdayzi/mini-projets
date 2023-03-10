55e4860367fee37179b93ee069e97a1d3c6d63da
sheluvdayzi <ziyadclombe.pro@gmail.com>
2023-03-10 09:16:09 +0100

    carousel terminé

--- intermediaire/Carousel/assets/js/script.js
+++ intermediaire/Carousel/assets/js/script.js
@@ -8,22 +8,9 @@ let firstindice = 0;
 let containerCards = document.querySelector('.container__cards');
 let translateX = 0;
 
-// getComputedStyle
-// console.log(cardGrey.style.width);
-// let a = cardGrey.getAttribute(width);
-// console.log(a);
-// let style = window.getComputedStyle(cardGrey);
-// console.log(style);
-
-// let style = window.getComputedStyle(cardGrey).getPropertyValue("width");
-// console.log(style);
-
 for (let i = 0; i < cards.length; i++) {
     let style = window.getComputedStyle(cards[i]).getPropertyValue("width");
-    // console.log(style);
 }
-
-
 
 arrowLeft.addEventListener('click', () => {
     let item = cards[firstindice-1].offsetWidth + Math.floor(window.innerWidth/100);
@@ -41,5 +28,4 @@ arrowRight.addEventListener('click', () => {
     console.log(firstindice)
     console.log(translateX);
     containerCards.style.transform = `translateX(-${translateX}px)`;
-    // item.style.transition = "width 5s ease-in-out 0s";
 });
